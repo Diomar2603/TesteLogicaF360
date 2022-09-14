f181fb94831d10d9e7eaf34b675f3fcd5ba43a2d
Leandro Diomar <leandrodiomar123@gmail.com>
2022-09-14 19:49:11 -0300

    Implementação do DownloadService

--- LogicaF360/Program.cs
+++ LogicaF360/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using LogicaF360.Services;
+
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+
+        var downloadService = new DownloadService();
+
+        string txtEscolhido;
+        do
+        {
+            Console.Clear();
+            Console.WriteLine("Por favor, escolha o texto (A ou B): ");
+            txtEscolhido = Console.ReadLine().Trim().ToUpper();
+        } while (!txtEscolhido.Equals("A") && !txtEscolhido.Equals("B"));
+
+        downloadService.BaixarTextos(txtEscolhido);
+
+    }
+}
---
+++ LogicaF360/Services/DownloadService.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicaF360.Services
+{
+    internal class DownloadService
+    {
+
+        //Links fixos dos textos
+        private const string LinkTextoA = "https://raw.githubusercontent.com/f360-dev/provas/master/klingon-textoA.txt";
+        private const string LinkTextoB = "https://raw.githubusercontent.com/f360-dev/provas/master/klingon-textoB.txt";
+
+        //Caminho das pastas
+        private static string PastaTextos = Directory.GetCurrentDirectory().Split("\\LogicaF360\\").GetValue(0)
+                          + "\\LogicaF360\\Resources\\KlingonTexts";
+
+        
+
+        public void BaixarTextos(string txtEscolhido)
+        {
+
+            var caminhoTexto = Path.Combine(PastaTextos, "klingon-texto" + txtEscolhido + ".txt");
+
+            if (!File.Exists(caminhoTexto))
+            {
+                Console.WriteLine("O Texto não se encontra na maquina, iremos fazer o Dowload pra você ;)");
+                try
+                {
+                    using (WebClient client = new WebClient())
+                    {
+                        switch (txtEscolhido)
+                        {
+                            case "A":
+                                client.DownloadFile(new Uri(LinkTextoA), caminhoTexto);
+                                break;
+
+                            case "B":
+                                client.DownloadFile(new Uri(LinkTextoB), caminhoTexto);
+                                break;
+                        }
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Erro disparado ao tentar Baixar texto: " + e.Message);
+                }
+            }
+            else
+            {
+                Console.WriteLine("O Texto já se encontra na maquina");
+            }
+        }
+    }
+}
